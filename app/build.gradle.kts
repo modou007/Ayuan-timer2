3d62864977dfeaa10a8c948cb87d0104b6954cac
WorkBuddy <workbuddy@example.com>
2026-08-13 21:08:33 +0800

    Fix CI: upgrade Java target to 17 and increase Gradle memory for AGP 8.2 compatibility

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,13 +24,13 @@ android {
         kotlinCompilerExtensionVersion = "1.5.8"
     }
 
-    kotlinOptions {
-        jvmTarget = "1.8"
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
 
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+    kotlinOptions {
+        jvmTarget = "17"
     }
 }
 
